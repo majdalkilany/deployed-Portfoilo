edae8603a0c862cbe7543a8368a5ab380bf34d69
majdalkilany <majdkilany922@gmail.com>
2020-04-04 15:36:37 +0300

    first serve

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const serve = express();
 
 const PORT = process.env.PORT|| 3000;
 
-serve.use(express.static('./public'));
-
 
 http://localhost:3000/test
 serve.get('/test',(request,response)=>{
+
+    serve.use(express.static('./public'));
+
 response.send('my name is majd')
 })
+
 
 
 serve.listen(PORT,()=>{
